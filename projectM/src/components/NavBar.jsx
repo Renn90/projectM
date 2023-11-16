01e88b23cc403bae102b9f6be30a6d697c2214dd
faithful <faithfulokondu@gmail.com>
2023-11-16 09:50:41 +0100

    layout

--- projectM/src/components/NavBar.jsx
+++ projectM/src/components/NavBar.jsx
@@ -9,9 +9,9 @@ const NavBar = () => {
         <img src={logo} className="w-[50px] ml-2 p-2" />
         <hr />
       </div>
-      <span className="flex items-center mr-[20px]">
-        <BiSearch />
-        <input type="search" className="border-[red]" placeholder="Search for a project"/>
+      <span className="flex items-center mr-[20px] p-[4px] border-[blue] border-[1px] rounded-md">
+        <BiSearch className="text-[blue] mx-2"/>
+        <input type="search" className="border-[red] outline-0" placeholder="Search for a project"/>
       </span>
     </div>
   );
--- projectM/src/components/SideNav.jsx
+++ projectM/src/components/SideNav.jsx
@@ -1,23 +1,34 @@
-import React from 'react';
-import { BiChat, BiHome, BiPin } from "react-icons/bi";
+import React, { useState } from 'react';
+import { BiChat, BiChevronLeft, BiChevronRight, BiHome, BiPin } from "react-icons/bi";
 
 const SideNav = () => {
-  const span = 'flex items-center text-black text-xl justify-start p-4 cursor-pointer hover:bg-[lightgrey] w-[100%]';
-  const anchor = 'ml-2'
+
+  const [toggleBar, setToggleBar] = useState(false)
+  const toggleHandler =()=> {
+    setToggleBar(!toggleBar)
+  }
+
+  const span = 'flex items-center text-black text-xl justify-start p-4 h-[60px] cursor-pointer hover:bg-[lightgrey] w-[100%]';
+  const anchor = `ml-2 ${toggleBar && 'hidden'}`
+  const icon = `${toggleBar && 'w-[100%]'}`
+  const chevron = 'absolute cursor-pointer text-2xl h-[60px] right-[-20px] top-0 bg-[white] hover:text-[grey]'
   return (
-    <nav className='flex flex-col items-start bg-white h-[100vh] w-[20%]'>
+    <nav className={`flex relative ${toggleBar ? 'sidebar' : 'sidebar_close'} h-[100vh] bg-white `}>
+    <div className='flex overflow-hidden flex-col items-start w-full'>
       <span className={span}>
-        <BiHome />
+        <BiHome className={icon}/>
         <a className={anchor}>Home</a>
       </span>
       <span className={span}>
-        <BiChat />
+        <BiChat className={icon}/>
         <a className={anchor}>Chat&nbsp;Room</a>
       </span>
       <span className={span}>
-        <BiPin />
+        <BiPin className={icon}/>
         <a className={anchor}>Pinned</a>
       </span>
+    </div>
+   {toggleBar ? <BiChevronRight className={chevron}  onClick={toggleHandler}/> : <BiChevronLeft className={chevron}  onClick={toggleHandler}/>}
     </nav>
   )
 }
